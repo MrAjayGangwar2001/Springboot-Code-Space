5a749efa1668abce25a876ed7011a8b8d7f85004
Ajay <gangwar030@gmail.com>
2025-11-11 01:18:57 +0530

    CompleteSpringJWTBackendProject

---
+++ SpringSecurity/SpringWebToken/src/test/java/com/Security/SpringWebToken/SpringWebTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Security.SpringWebToken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
