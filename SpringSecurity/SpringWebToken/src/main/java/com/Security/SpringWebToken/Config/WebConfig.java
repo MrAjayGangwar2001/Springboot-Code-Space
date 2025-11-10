7490eb057611ba6ce562f493f0bb578187f06e1a
Ajay <gangwar030@gmail.com>
2025-11-10 23:27:17 +0530

    WebConfigFile

---
+++ SpringSecurity/SpringWebToken/src/main/java/com/Security/SpringWebToken/Config/WebConfig.java
@@ -0,0 +1,44 @@
+package com.Security.SpringWebToken.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.Security.SpringWebToken.Auth.JWTChecker;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig {
+
+    private final JWTChecker jwtcheck;
+
+    @Bean
+    public PasswordEncoder EncodePass(){
+
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain SecurityConfig(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                    .csrf(csrf -> csrf.disable())
+                    .authorizeHttpRequests(auth -> auth
+                                                    .requestMatchers("/", "/login", "/register").permitAll()
+                                                    .requestMatchers("/user", "/admin").authenticated()
+                                                    .anyRequest().authenticated()
+                    )
+                    .formLogin(form -> form.disable())
+                    .logout(logout -> logout.disable())
+                    .httpBasic(basic -> basic.disable())
+                    .addFilterBefore(jwtcheck, UsernamePasswordAuthenticationFilter.class);
+                    
+                    return httpSecurity.build();
+    
+    }
+}
