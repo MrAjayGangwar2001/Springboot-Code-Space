34e98ce86c776dec37c23c24df99d1a6910e345b
Ajay <gangwar030@gmail.com>
2025-11-11 01:15:12 +0530

    MainFile

---
+++ SpringSecurity/SpringWebToken/src/main/java/com/Security/SpringWebToken/SpringWebTokenApplication.java
@@ -0,0 +1,14 @@
+package com.Security.SpringWebToken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebTokenApplication.class, args);
+		System.out.println("SPRING SECURITY JWT APPLICATION IS RUNNING ✅");
+	}
+
+}
