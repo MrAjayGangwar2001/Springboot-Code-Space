9d4840cb614e007afc6b141deaa41eb4b9cf83e7
Ajay <gangwar030@gmail.com>
2025-11-10 23:28:40 +0530

    HomeController

---
+++ SpringSecurity/SpringWebToken/src/main/java/com/Security/SpringWebToken/Controller/HomeController.java
@@ -0,0 +1,54 @@
+package com.Security.SpringWebToken.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Security.SpringWebToken.Dto.LoginDto;
+import com.Security.SpringWebToken.Model.UserModel;
+import com.Security.SpringWebToken.Service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@RequiredArgsConstructor
+public class HomeController {
+
+    private final UserService userv;
+
+    @GetMapping("/")
+    public String Home() {
+        return "YOU ARE ON HOME PAGE , NO LOGIN NEEDED";
+    }
+
+    @GetMapping("/user")
+    public String UserHome() {
+        return "YOU ARE ON USER HOME PAGE.";
+    }
+    @GetMapping("/admin")
+    public String AdminHome() {
+        return "YOU ARE ON ADMIN hOME PAGE.";
+    }
+
+    @GetMapping("/verify")
+    public String verify() {
+        return "USER HAS BEEN VERIFIED BY TOKEN.";
+    }
+
+    @PostMapping("/register")
+    public String RegisterUser(@RequestBody UserModel user) {
+       
+        return userv.CreateUser(user);
+    }
+
+    @PostMapping("/login")
+    public String UserLogin(@RequestBody LoginDto login) {
+        
+        return userv.LoginUser(login);
+    }
+    
+}
