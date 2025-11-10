7874a145437cccca98423c8b00eb297e117e2800
Ajay <gangwar030@gmail.com>
2025-11-10 23:30:15 +0530

    UserModel

---
+++ SpringSecurity/SpringWebToken/src/main/java/com/Security/SpringWebToken/Model/UserModel.java
@@ -0,0 +1,45 @@
+package com.Security.SpringWebToken.Model;
+
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Data
+@Getter
+@Setter
+@Table(name = "users")
+public class UserModel implements UserDetails{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(unique = true)
+    private String email;
+    @Column(unique = true)
+    private String userName;
+    private String password;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+       return List.of();
+    }
+    @Override
+    public String getUsername() {
+    //    return getEmail();
+        return this.email;
+    }
+}
