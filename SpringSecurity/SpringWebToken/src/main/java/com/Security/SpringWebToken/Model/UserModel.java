df252b304205dbe6ba57fe8603f534b7247ad3bd
Ajay <gangwar030@gmail.com>
2025-11-22 23:59:30 +0530

    UpdateUserModel

--- SpringSecurity/SpringWebToken/src/main/java/com/Security/SpringWebToken/Model/UserModel.java
+++ SpringSecurity/SpringWebToken/src/main/java/com/Security/SpringWebToken/Model/UserModel.java
@@ -5,22 +5,29 @@ import java.util.Collection;
 import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import com.Security.SpringWebToken.Enum.Role;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Data
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "users")
 public class UserModel implements UserDetails{
 
@@ -33,13 +40,22 @@ public class UserModel implements UserDetails{
     @Column(unique = true)
     private String userName;
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-       return List.of();
+       return List.of(new SimpleGrantedAuthority("ROLE_" +role.name()));
     }
     @Override
     public String getUsername() {
     //    return getEmail();
-        return this.email;
+        // return this.email;
+        return this.userName;
+    }
+    @Override
+    public String getPassword() {
+        return this.password;
     }
 }
