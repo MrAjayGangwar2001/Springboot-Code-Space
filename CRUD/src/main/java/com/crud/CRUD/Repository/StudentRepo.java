3147e7c5898b38d6a6b9472355b351be23043b31
Ajay <gangwar030@gmail.com>
2025-11-24 23:51:09 +0530

    StudentRepo

---
+++ CRUD/src/main/java/com/crud/CRUD/Repository/StudentRepo.java
@@ -0,0 +1,10 @@
+package com.crud.CRUD.Repository;
+
+import com.crud.CRUD.Model.StudentModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepo extends JpaRepository<StudentModel, Integer> {
+
+}
