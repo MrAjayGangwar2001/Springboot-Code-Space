9e1c2a33759f6c534911d66a92d26f7a5aac087b
Ajay <gangwar030@gmail.com>
2025-12-27 03:04:28 +0530

    SecurityConfig

---
+++ Login_System_JWt-OAuth/LoginBackend/src/main/java/com/OTP_Login/Config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.OTP_Login.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http){
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/email/**",
+                                "/login/**",
+                                "/oauth2/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth -> oauth
+                        .defaultSuccessUrl("/api/email/oauth2/success", true)
+                )
+                .formLogin(login -> login.disable())
+                .httpBasic(basic -> basic.disable());
+
+        return http.build();
+    }
+}
