0e57da8f7cf23542bebc3a0e690678f86af2563a
Ajay <gangwar030@gmail.com>
2025-12-27 17:00:52 +0530

    JWTUtil file.java

---
+++ Login_System_JWt-OAuth/LoginBackend/src/main/java/com/OTP_Login/JWT/JwtUtil.java
@@ -0,0 +1,33 @@
+package com.OTP_Login.JWT;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final long EXPIRY = 1000 * 60 * 1; 
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRY))
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
