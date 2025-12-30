60db1fadfc21c1c8bbbae08666f51356f0591985
Ajay <gangwar030@gmail.com>
2025-12-30 13:45:10 +0530

    Service Layer

---
+++ ClassRestfullApi/src/main/java/com/example/RestfullApi/service/logic/StudentService.java
@@ -0,0 +1,21 @@
+package com.example.RestfullApi.service.logic;
+
+import org.springframework.stereotype.Service;
+
+import com.example.RestfullApi.Dto.StudentDto;
+
+@Service
+public class StudentService {
+
+
+
+
+    public StudentDto createStudent (StudentDto studentDto){
+
+//  dto tempory // permanet datase  => database connect 
+
+
+        return studentDto;
+        
+    }
+}
