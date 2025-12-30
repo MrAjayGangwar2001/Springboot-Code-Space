fda6c78d374ddde223291e85f5009fba437dce04
Ajay <gangwar030@gmail.com>
2025-12-30 13:46:19 +0530

    Main Class File

---
+++ ClassRestfullApi/src/main/java/com/example/RestfullApi/RestfullApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.RestfullApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullApiApplication.class, args);
+		System.out.println("hey i am live");
+	}
+
+}
