ac899ac41065c168a10b34ad4aabb7adfdc3ab6f
Ajay <gangwar030@gmail.com>
2025-12-30 13:50:03 +0530

    Restfull Api

---
+++ ClassRestfullApi/src/test/java/com/example/RestfullApi/RestfullApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestfullApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
