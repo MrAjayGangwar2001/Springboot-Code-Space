ffd9cf99d675328f2c7426cdc9541da0e2cf98d2
Ajay <gangwar030@gmail.com>
2026-01-02 23:31:14 +0530

    Student Course Entity  Model

---
+++ SMS/src/main/java/com/example/sms/model/Course.java
@@ -0,0 +1,36 @@
+package com.example.sms.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String courseName;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+}
+
+// @ManyToOne
+// @JoinColumn(name = "student_id") // forined key on course table
+// private Student student;
