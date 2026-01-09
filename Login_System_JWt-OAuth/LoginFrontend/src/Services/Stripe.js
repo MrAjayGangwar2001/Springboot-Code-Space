7a8b786785d9d976104cba9d8a435443d772da24
Ajay <gangwar030@gmail.com>
2026-01-09 23:46:48 +0530

    Stripe Plateform

---
+++ Login_System_JWt-OAuth/LoginFrontend/src/Services/Stripe.js
@@ -0,0 +1,5 @@
+import { loadStripe } from "@stripe/stripe-js";
+
+export const stripePromise = loadStripe(
+    "pk_test_51SnP2SAmr9lWiySP2lSVbeC51s97smR67aJKh8qKAcEGMyjQBumcbxHCkiTjj0IjZ9XXIHGP3FieIpTnxBNRF0jM00vnyY6Dau"
+);
