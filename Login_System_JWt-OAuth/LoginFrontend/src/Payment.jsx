3c26fb5f30362e7f7c3b46873bef3258ed884570
Ajay <gangwar030@gmail.com>
2026-01-12 00:45:53 +0530

    Updated Payment"

--- Login_System_JWt-OAuth/LoginFrontend/src/Payment.jsx
+++ Login_System_JWt-OAuth/LoginFrontend/src/Payment.jsx
@@ -5,9 +5,22 @@ export default function Payment() {
 
    const handlePayment = async () => {
       console.log("Enter Handle click Button");
-      
+
+      const amount = document.getElementById("amount").value;
+
+      if (!amount || amount <= 0) {
+         alert("Minimum Amount should be 10");
+         return;
+      }
+
       const res = await fetch("http://localhost:8080/api/payment/create", {
          method: "POST",
+         headers: {
+            "Content-Type": "application/json",
+         },
+         body: JSON.stringify({
+            amount: Number(amount), email// ₹
+         }),
       });
       console.log("After API Fetch");
 
@@ -20,16 +33,28 @@ export default function Payment() {
    };
 
    return (
-      <button
-         style={{
-            padding: "12px 20px",
-            background: "#0f172a",
-            color: "#fff",
-            borderRadius: "6px",
-            marginTop: "20px"
-         }}
-         onClick={handlePayment}>
-         💳 Make Payment
-      </button>
+
+      <div style={{ textAlign: "center" }}>
+         <h2>Enter Amount (₹)</h2>
+         <input
+            id="amount"
+            type="number"
+            placeholder="Enter amount"
+            style={{ padding: "10px", margin: "10px" }}
+         />
+         <br />
+
+         <button
+            style={{
+               padding: "12px 20px",
+               background: "#0f172a",
+               color: "#fff",
+               borderRadius: "6px",
+               marginTop: "20px"
+            }}
+            onClick={handlePayment}>
+            💳 Make Payment
+         </button>
+      </div>
    );
 }
