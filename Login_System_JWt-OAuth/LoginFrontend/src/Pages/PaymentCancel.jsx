3cdfcc95702c436ec86b8ce713a276b1cd703835
Ajay <gangwar030@gmail.com>
2026-01-12 00:44:31 +0530

    Updated File

---
+++ Login_System_JWt-OAuth/LoginFrontend/src/Pages/PaymentCancel.jsx
@@ -0,0 +1,8 @@
+export default function PaymentCancel() {
+    return (
+        <div style={{ textAlign: "center", marginTop: "80px" }}>
+            <h1>❌ Payment Cancelled</h1>
+            <p>Payment was not completed</p>
+        </div>
+    );
+}
